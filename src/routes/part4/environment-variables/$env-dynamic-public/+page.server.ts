f5d5d2b0ef107a7907b1219bbfaa9572ed715525
Shin HyunJae <shyunjan@naver.com>
2024-03-20 21:52:24 +0900

    fix bug 16

---
+++ src/routes/part4/environment-variables/$env-dynamic-public/+page.server.ts
@@ -0,0 +1,8 @@
+import { env } from '$env/dynamic/public';
+
+export function load() {
+  return { env: { 
+    PUBLIC_THEME_BACKGROUND: env.PUBLIC_THEME_BACKGROUND,
+    PUBLIC_THEME_FOREGROUND: env.PUBLIC_THEME_FOREGROUND,
+  }};
+}
--- src/routes/part4/environment-variables/$env-dynamic-public/+page.svelte
+++ src/routes/part4/environment-variables/$env-dynamic-public/+page.svelte
@@ -1,9 +1,11 @@
 <script>
-  import { env } from '$env/dynamic/public';
+  // import { env } from '$env/dynamic/public';
+  // 위 방식은 dev 모드에서는 작동하지만 vercel에 배포하고 빌드했을 경우에는 작동하지 않아서 ./+page.server.ts와 함께 아래 방식을 사용한다.
+  export let data;
 </script>
 
-<main style:background={env.PUBLIC_THEME_BACKGROUND} style:color={env.PUBLIC_THEME_FOREGROUND}>
-  {env.PUBLIC_THEME_FOREGROUND} on {env.PUBLIC_THEME_BACKGROUND}
+<main style:background={data?.env.PUBLIC_THEME_BACKGROUND} style:color={data.env.PUBLIC_THEME_FOREGROUND}>
+  {data.env.PUBLIC_THEME_FOREGROUND} on {data.env.PUBLIC_THEME_BACKGROUND}
 </main>
 
 <style>
