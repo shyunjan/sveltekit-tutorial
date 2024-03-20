354b04ab521db2f3689108ac85622dddd9445662
Shin HyunJae <shyunjan@naver.com>
2024-03-20 19:54:50 +0900

    fix bug 7

--- src/routes/part4/environment-variables/$env-static-private/+page.server.ts
+++ src/routes/part4/environment-variables/$env-static-private/+page.server.ts
@@ -1,5 +1,5 @@
 import { redirect, fail } from '@sveltejs/kit';
-import { PASSPHRASE } from '$env/static/private';
+// import { PASSPHRASE } from '$env/static/private';
 
 export function load({ cookies }) {
   if (cookies.get('allowed')) {
@@ -9,18 +9,18 @@ export function load({ cookies }) {
 
 export const actions = {
   default: async ({ request, cookies }) => {
-    const data = await request.formData();
+    // const data = await request.formData();
 
-    if (data.get('passphrase') === PASSPHRASE) {
-      cookies.set('allowed', 'true', {
-        path: '/part4/environment-variables'
-      });
+    // if (data.get('passphrase') === PASSPHRASE) {
+    //   cookies.set('allowed', 'true', {
+    //     path: '/part4/environment-variables'
+    //   });
 
-      throw redirect(303, '/part4/environment-variables/$env-static-private/welcome');
-    }
+    //   throw redirect(303, '/part4/environment-variables/$env-static-private/welcome');
+    // }
 
-    return fail(403, {
-      incorrect: true
-    });
+    // return fail(403, {
+    //   incorrect: true
+    // });
   }
 };
