67d93c07b600ef51dd9d387de621ce58f9550c50
Shin HyunJae <shyunjan@naver.com>
2024-03-20 20:10:37 +0900

    fix bug 9

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
     adapter: 
       // adapter(), // as using @sveltejs/adapter-auto
       adapter({
-        runtime: 'nodejs18.x',
+        runtime: 'nodejs20.x',
         // maxDuration: // Defaults to 10 seconds for Hobby accounts, 15 for Pro and 900 for Enterprise
       }),
 
