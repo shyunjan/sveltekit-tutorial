af65508e515fbdcf099ab0e6774407d2247da8c0
Shin HyunJae <shyunjan@naver.com>
2024-03-20 17:23:24 +0900

    add @sveltejs/adapter-vercel module and add it to svelte.config.js for production build to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel'
+
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +13,12 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: 
+      // adapter(), // as using @sveltejs/adapter-auto
+      adapter({
+        runtime: 'edge',
+        // maxDuration: // Defaults to 10 seconds for Hobby accounts, 15 for Pro and 900 for Enterprise
+      }),
 
 		version: {
 			// ideally, this should be something deterministic like the output of `git rev-parse HEAD`
