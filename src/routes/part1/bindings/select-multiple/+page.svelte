aa77e359bf97e4d7e3662868b44a6b0b42374215
Shin HyunJae <shyunjan@naver.com>
2024-05-30 08:11:14 +0900

    complete renewal for Part1 > Events > Select multiple

--- src/routes/part1/bindings/select-multiple/+page.svelte
+++ src/routes/part1/bindings/select-multiple/+page.svelte
@@ -1,44 +1,43 @@
 <script lang="ts">
-	let scoops = 1;
-	let flavours: string[] = [];
+  let scoops = 1;
+  let flavours: string[] = [];
 
-	const formatter = new Intl.ListFormat('en', { style: 'long', type: 'conjunction' });
+  const formatter = new Intl.ListFormat('en', { style: 'long', type: 'conjunction' });
+
+  import Tutorial from './Tutorial.md';
 </script>
 
 <h2>Size</h2>
 
 {#each [1, 2, 3] as number}
-	<label>
-		<input
-			type="radio"
-			name="scoops"
-			value={number}
-      bind:group={scoops}
-		/>
+  <label>
+    <input type="radio" name="scoops" value={number} bind:group={scoops} />
 
-		{number} {number === 1 ? 'scoop' : 'scoops'}
-	</label>
+    {number}
+    {number === 1 ? 'scoop' : 'scoops'}
+  </label>
 {/each}
 
 <h2>Flavours</h2>
 
 <select multiple bind:value={flavours}>
-	{#each ['cookies and cream', 'mint choc chip', 'raspberry ripple'] as flavour}
-		<option>{flavour}</option>
-	{/each}
+  {#each ['cookies and cream', 'mint choc chip', 'raspberry ripple'] as flavour}
+    <option>{flavour}</option>
+  {/each}
 </select>
-<!-- 
-	NOTE
-	Press and hold the control key (or the command key on MacOS) to select multiple options.
--->
 
 {#if flavours.length === 0}
-	<p>Please select at least one flavour</p>
+  <p>Please select at least one flavour</p>
 {:else if flavours.length > scoops}
-	<p>Can't order more flavours than scoops!</p>
+  <p>Can't order more flavours than scoops!</p>
 {:else}
-	<p>
-		You ordered {scoops} {scoops === 1 ? 'scoop' : 'scoops'}
-		of {formatter.format(flavours)}
-	</p>
+  <p>
+    You ordered {scoops}
+    {scoops === 1 ? 'scoop' : 'scoops'}
+    of {formatter.format(flavours)}
+  </p>
 {/if}
+
+<div class="tutorial">
+  <Tutorial />
+</div>
