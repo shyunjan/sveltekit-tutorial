73f7a689459bb9001d98bf9301f176d373ebbf3c
Shin HyunJae <shyunjan@naver.com>
2024-05-31 20:26:18 +0900

    complete renewal for Part1 > Lifecycle > tick

--- src/routes/part1/lifecycle/tick/+page.svelte
+++ src/routes/part1/lifecycle/tick/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { tick } from 'svelte';
+
   let text = `Select some text and hit the tab key to toggle uppercase`;
 
   async function handleKeydown(event: KeyboardEvent) {
@@ -13,17 +14,27 @@
     const replacement = /[a-z]/.test(selection) ? selection.toUpperCase() : selection.toLowerCase();
 
     text = value.slice(0, selectionStart) + replacement + value.slice(selectionEnd);
-    /* text 변수를 변경했으므로 reactivity가 발동해서 DOM(<textarea />)이 변경되어야 하지만, 
+    /* text 변수를 변경했으므로 reactivity가 발동해서 DOM(<textarea />)이 변경되어야 하지만,
      * 원칙적으로는 이 함수(handleKeydown())가 끝날 때까지는 업데이트되지 않는다. */
 
     await tick(); // tick 함수는 DOM 업데이트가 끝날 때까지 기다린 다음 아래 라인들을 실행시킨다.
     this.selectionStart = selectionStart;
     this.selectionEnd = selectionEnd;
   }
+
+  import Tutorial from './Tutorial.md';
 </script>
 
 <textarea value={text} on:keydown={handleKeydown} />
 
+<div class="tutorial">
+  <Tutorial />
+</div>
+
 <style>
-  textarea { width: 100%; height: 100%; resize: none; }
+  textarea {
+    width: 100%;
+    height: 100%;
+    resize: none;
+  }
 </style>
