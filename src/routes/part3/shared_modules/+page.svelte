90737dcfa86c6ae60e5997e525f5f66f6e2e5c97
Shin HyunJae <shyunjan@naver.com>
2024-06-28 10:50:54 +0900

    complete renewal for Part3 > Shared modules > The  alias

--- src/routes/part3/shared_modules/+page.svelte
+++ src/routes/part3/shared_modules/+page.svelte
@@ -1,11 +1,16 @@
 <script>
   import { message } from '$lib/message.js';
+  import Tutorial from './Tutorial.md';
 </script>
 
-<nav>
-  <a href="/part3/shared_modules">The $lib alias</a>
+<nav class="relative mb-6 flex gap-6 rounded bg-stone-100 p-6">
+  <a href="/part3/shared_modules">home</a>
   <a href="/part3/shared_modules/a/deeply/nested/route">a deeply nested route</a>
 </nav>
-<br/>
-<h1>The $lib alias</h1>
+<br />
+<h1>Home</h1>
 <p>{message}</p>
+
+<div class="tutorial">
+  <Tutorial />
+</div>
--- src/routes/part3/shared_modules/a/deeply/nested/route/+page.svelte
+++ src/routes/part3/shared_modules/a/deeply/nested/route/+page.svelte
@@ -2,11 +2,10 @@
   import { message } from '$lib/message.js';
 </script>
 
-<nav>
-  <a href="/part3/shared_modules">The $lib alias</a>
+<nav class="relative mb-6 flex gap-6 rounded bg-stone-100 p-6">
+  <a href="/part3/shared_modules">home</a>
   <a href="/part3/shared_modules/a/deeply/nested/route">a deeply nested route</a>
 </nav>
-<br/>
-<h1>a deeply nested route</h1>
+<br />
+<h1>A deeply nested route</h1>
 <p>{message}</p>
-
