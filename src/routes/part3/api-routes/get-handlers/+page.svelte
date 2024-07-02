4aeedb7420baf8453e062f3bd3cfc9d258aebb02
Shin HyunJae <shyunjan@naver.com>
2024-07-02 13:58:39 +0900

    complete renewal for Part3 > API routes > GET handlers

--- src/routes/part3/api-routes/get-handlers/+page.svelte
+++ src/routes/part3/api-routes/get-handlers/+page.svelte
@@ -6,6 +6,8 @@
     const response = await fetch('/part3/api-routes/get-handlers/roll');
     number = await response.json();
   }
+
+  import Tutorial from './Tutorial.md';
 </script>
 
 <button on:click={roll}>Roll the dice</button>
@@ -13,3 +15,7 @@
 {#if number !== undefined}
   <p>You rolled a {number}</p>
 {/if}
+
+<div class="tutorial absolute top-[10%]">
+  <Tutorial />
+</div>
