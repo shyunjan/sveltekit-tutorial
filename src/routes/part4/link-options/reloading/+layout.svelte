e552be2713b125cf696b133854eb6c4579cce318
Shin HyunJae <shyunjan@naver.com>
2024-07-12 12:30:21 +0900

    complete renewal for Part4 > Link options > Reloading the page

--- src/routes/part4/link-options/reloading/+layout.svelte
+++ src/routes/part4/link-options/reloading/+layout.svelte
@@ -12,9 +12,11 @@
       clearInterval(interval);
     };
   });
+
+  import Tutorial from './Tutorial.md';
 </script>
 
-<nav data-sveltekit-reload>
+<nav data-sveltekit-reload class="relative mb-6 flex gap-6 rounded bg-stone-100 p-6">
   <a href="/part4/link-options/reloading">Reloading the page &gt; home</a>
   <a href="/part4/link-options/reloading/test_for_reloading">reloading test</a>
 </nav>
@@ -24,21 +26,5 @@
 <p>the page has been open for {seconds} seconds</p>
 
 <div class="tutorial">
-  <p>Ordinarily, SvelteKit will navigate between pages without refreshing the page.
-    In this exercise, if we navigate between <code>./</code> and <code>./test_for_reloading</code>,
-    the timer keeps on ticking.
-  </p>
-  <p>In rare cases, you might want to disable this behaviour. You can do so by adding the
-    <code>data-sveltekit-reload</code> attribute on an individual link, or any element that
-    contains links:
-  </p>
-  <div class="code-block">
-    <p>See <code>.\+layout.svelte</code></p>
-    <pre class="language-svelte"><code><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>nav</span> <span class="highlight add"><span class="token attr-name">data-sveltekit-reload</span></span><span class="token punctuation">&gt;</span></span> ... <span class="token tag"><span class="token punctuation">&lt;/</span>nav<span class="token punctuation">&gt;</span></span></code></pre>
-  </div>
-  <p>For more information on available link options and their values, consult the
-    <a href="https://kit.svelte.dev/docs/link-options" target="_blank">link options
-      documentation
-    </a>.
-  </p>
+  <Tutorial />
 </div>
